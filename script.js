52e29fcfea5ccb292393913370ca904b7cf5c92e
Daniel Krummhauer <d.krummhauer@gmail.com>
2025-12-28 00:04:48 +0100

    add function scaleIngredient and modify function calculatePortion

--- script.js
+++ script.js
@@ -17,27 +17,65 @@ function sendMail(event){﻿
     });
 }
 
-// create variable currently amount -> showes currently amount as a string of ingredient
-let currentlyAmount = document.getElementById('amount_mincemeat').innerHTML;
-    // console.log(currentlyAmount);
-
-// function calculatePortion: Calculates the amount of ingredient
-function calculatePortion() {
+// function calculatePortion(): Calculates the amount of ingredient after "onclick" on button with class = button
+function calculatePortion(id, referenceAmount) {
+   
     // create variable myInput and read value of id = amount 
     let myInput = document.getElementById("amount").value;
-    // console.log(myInput);
 
     // create variable basic portion for four (4) portions
     let basicPortion = 4;
-
-    let referenceAmount = 500;
-    let result = (referenceAmount/basicPortion)*myInput;
+    
+    // calculates new amount of ingredient depending of portion (generally -> because of function parameter: referenceAmount)
+    let newAmount = (referenceAmount/basicPortion)*myInput;
     
     // to log onclick on button, set console.log(result) here
-    console.log(result);
-    return result;
+    // console.log(result);
+
+    // writes calculated amount in div container with id (generally -> because of function parameter: id)
+    document.getElementById(id).innerHTML = newAmount;
+    
+    // finish of function calcuatePortion ()
+    return newAmount;
 };
 
+// function scaleIngredient(): Scales all ingredients with new amount after "onclick" on button with class = button
+// therefore referenceAmount of all ingredient-ids are necessary
+function scaleIngredient() {
+
+    // scales "Rinderhackfleisch" 
+    calculatePortion("amount_mincemeat", 500);
+
+    // scales "passierte Tomaten" 
+    calculatePortion("amount_tomato", 800);
+
+    // scales "Paprikaschote" 
+    calculatePortion("amount_paprika", 1);
+
+    // scales "Paprikaschote" 
+    calculatePortion("amount_paprika", 1);
+
+    // scales "Knoblauchzehe" 
+    calculatePortion("amount_garlic", 1);
+
+    // scales "Kidneybohnen" 
+    calculatePortion("amount_beans", 1);
+    
+    // scales "Mais" 
+    calculatePortion("amount_corn", 1);
+
+    // scales "Zwiebel" 
+    calculatePortion("amount_onion", 1);
+
+    // scales "Tomatenmark" 
+    calculatePortion("amount_tomatopaste", 50);
+
+    // scales "Paprikapulver"
+    calculatePortion("amount_paprikapowder", 2);
+
+    // scales "Olivenöl"
+    calculatePortion("amount_oliveoil", 2);
+}
+
 // next:
-// - write result into html script
 // - if else for value-borders (1 .... 20)
