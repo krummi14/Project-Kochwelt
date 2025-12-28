d8252798a27e3e7d9d1bb2b7cd1f919f7d87904a
Daniel Krummhauer <d.krummhauer@gmail.com>
2025-12-28 22:29:01 +0100

    add function checkValue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // function for contact.html
 // sendMail()
-function sendMail(event){﻿
+function sendMail(event) {
     event.preventDefault();
     const data = new FormData(event.target);
 
@@ -18,64 +18,89 @@ function sendMail(event){﻿
 }
 
 // function calculatePortion(): Calculates the amount of ingredient after "onclick" on button with class = button
-function calculatePortion(id, referenceAmount) {
-   
+function calculateAmount(id, referenceAmount) {
+
     // create variable myInput and read value of id = amount 
     let myInput = document.getElementById("amount").value;
 
     // create variable basic portion for four (4) portions
     let basicPortion = 4;
-    
+
     // calculates new amount of ingredient depending of portion (generally -> because of function parameter: referenceAmount)
-    let newAmount = (referenceAmount/basicPortion)*myInput;
-    
+    let newAmount = (referenceAmount / basicPortion) * myInput;
+
     // to log onclick on button, set console.log(result) here
     // console.log(result);
 
     // writes calculated amount in div container with id (generally -> because of function parameter: id)
     document.getElementById(id).innerHTML = newAmount;
-    
+
     // finish of function calcuatePortion ()
     return newAmount;
 };
+
+// function checkValue(): is checking value of myInput. Min=1 and Max=20 
+// -> If borders are not fully observed, the calue does not change (corresponds for four portions)
+function checkValue(id, referenceAmount) {
+
+    // create variable myInput and read value of id = amount
+    let myInput = document.getElementById("amount").value;
+    let form = document.getElementById('portion_form');
+
+
+    if (myInput < 1 || myInput > 20) {
+    
+        // writes referenceAmount into HTML
+        document.getElementById(id).innerHTML = referenceAmount;
+        console.log("Value is not valid");
+    }
+
+    else {
+        console.log("Value is valid");
+    }
+}
 
 // function scaleIngredient(): Scales all ingredients with new amount after "onclick" on button with class = button
 // therefore referenceAmount of all ingredient-ids are necessary
 function scaleIngredient() {
 
     // scales "Rinderhackfleisch" 
-    calculatePortion("amount_mincemeat", 500);
+    calculateAmount("amount_mincemeat", 500);
+    checkValue("amount_mincemeat", 500);
 
     // scales "passierte Tomaten" 
-    calculatePortion("amount_tomato", 800);
+    calculateAmount("amount_tomato", 800);
+    checkValue("amount_tomato", 800);
 
     // scales "Paprikaschote" 
-    calculatePortion("amount_paprika", 1);
-
-    // scales "Paprikaschote" 
-    calculatePortion("amount_paprika", 1);
+    calculateAmount("amount_paprika", 1);
+    checkValue("amount_paprika", 1);
 
     // scales "Knoblauchzehe" 
-    calculatePortion("amount_garlic", 1);
+    calculateAmount("amount_garlic", 1);
+    checkValue("amount_garlic", 1);
 
     // scales "Kidneybohnen" 
-    calculatePortion("amount_beans", 1);
-    
+    calculateAmount("amount_beans", 1);
+    checkValue("amount_beans", 1);
+
     // scales "Mais" 
-    calculatePortion("amount_corn", 1);
+    calculateAmount("amount_corn", 1);
+    checkValue("amount_corn", 1);
 
     // scales "Zwiebel" 
-    calculatePortion("amount_onion", 1);
+    calculateAmount("amount_onion", 1);
+    checkValue("amount_onion", 1);
 
     // scales "Tomatenmark" 
-    calculatePortion("amount_tomatopaste", 50);
+    calculateAmount("amount_tomatopaste", 50);
+    checkValue("amount_tomatopaste", 50);
 
     // scales "Paprikapulver"
-    calculatePortion("amount_paprikapowder", 2);
+    calculateAmount("amount_paprikapowder", 2);
+    checkValue("amount_paprikapowder", 2);
 
     // scales "Olivenöl"
-    calculatePortion("amount_oliveoil", 2);
+    calculateAmount("amount_oliveoil", 2);
+    checkValue("amount_oliveoil", 2);
 }
-
-// next:
-// - if else for value-borders (1 .... 20)
